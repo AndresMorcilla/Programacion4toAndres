36f48065380579b61b7f5b6141fd319935c0ec22
Andres <agmorillofigueroa@escuelasproa.edu.ar>
2025-04-14 11:43:01 -0300

    Update AndresAbrilUno.py

--- AndresAbrilUno.py
+++ AndresAbrilUno.py
@@ -2,11 +2,11 @@ print('Promediador de notas estudiantiles')
 print('Dicte sus notas y si quiere cerrar el programa ponga "salir"')
 notas=[]
 while True:
-  x=int(input('>>'))
+  x=input('>>')
   if x == 'salir':
     break
   else:
-    notas.append(x)
+    notas.append(int(x))
 resultado=0
 for nota in notas:
   resultado+=nota
